8105af17d4ab3b133d009c57aae97d54a90d76fc
thongcam <45142231+thongcam@users.noreply.github.com>
2019-06-23 19:31:42 +0700

    Updates

---
+++ precache-manifest.2ace95d5b45554b90eceb012d610df9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "991801c5f0919705b1465ba38bd091a8",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "165c83f85a4584eb65d7",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "e4f9458a94bf37e014aa",
+    "url": "/robofriends/static/css/main.e71ffac1.chunk.css"
+  },
+  {
+    "revision": "165c83f85a4584eb65d7",
+    "url": "/robofriends/static/js/2.590933e2.chunk.js"
+  },
+  {
+    "revision": "e4f9458a94bf37e014aa",
+    "url": "/robofriends/static/js/main.68825bf7.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
